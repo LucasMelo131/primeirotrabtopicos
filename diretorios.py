75130ad676f79d5d47b75bf5f781951f4ffe21a1
Lucas Melo de Carvalho <lucas_m@id.uff.br>
2023-05-28 20:08:05 -0300

    primeiro teste

--- diretorios.py
+++ diretorios.py
@@ -1,24 +1,26 @@
 """
-Faça um programa que leia uma string a qual representa um diretório. Gere um arquivo json com todos os arquivos deste diretório, dos seus diretórios filhos, dos filhos dos filhos, etc...
+Faça um programa que leia uma string a qual representa um diretório. 
+Gere um arquivo json com todos os arquivos deste diretório, 
+dos seus diretórios filhos, dos filhos dos filhos, etc...
 formato json: combinação de lista e dicionário
 ITEM : nome, lista com todos os arquivos, diretórios(lista)
 diretório é uma lista de ITEM
-OS -> PESQUISAR
 """
 
 import os
-import json
+#import json
+
 
 path = input("Entre com o caminho do diretório: ")
-diretorio = list()
-item = dict()
-
-for dpath,dirs,files in os.walk(path):
-    print(dpath)
-    for d in dirs:
-        print(d)
-    for f in files:
-        print(f)
-
-#list_dir = os.listdir(path)
-#print(list_dir)
+directory = []
+for dirpath,dirs,files in os.walk(path):
+    item = {}
+    item["Nome"] = os.path.basename(dirpath)
+    item["Arquivos"] = []
+    item["Diretorios"] = []
+    for file in files:
+        item["Arquivos"].append(file)
+    for folder in dirs:
+        item["Diretorios"].append(folder)
+    directory.append(item)
+print(directory)
