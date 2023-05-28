b70bf9f2a914004a6ef788a308387044112bfb8b
Lucas Melo de Carvalho <lucas_m@id.uff.br>
2023-05-28 20:36:08 -0300

    segundo teste

--- diretorios.py
+++ diretorios.py
@@ -10,17 +10,29 @@ diretório é uma lista de ITEM
 import os
 #import json
 
+def directories (caminho,lista):
+    """_summary_
+
+    Args:
+        caminho (_string_): _description_
+        lista (_list_): _description_
+
+    Returns:
+        _list_: _description_
+    """
+    for dirpath,dirs,files in os.walk(caminho):
+        item = {}
+        item["Nome"] = os.path.basename(dirpath)
+        item["Arquivos"] = []
+        item["Diretorios"] = []
+        for file in files:
+            item["Arquivos"].append(file)
+        for folder in dirs:
+            item["Diretorios"].append(folder)
+        lista.append(item)
+    return lista
 
 path = input("Entre com o caminho do diretório: ")
 directory = []
-for dirpath,dirs,files in os.walk(path):
-    item = {}
-    item["Nome"] = os.path.basename(dirpath)
-    item["Arquivos"] = []
-    item["Diretorios"] = []
-    for file in files:
-        item["Arquivos"].append(file)
-    for folder in dirs:
-        item["Diretorios"].append(folder)
-    directory.append(item)
+directory = directories(path,directory)
 print(directory)
