da221478d111981587ef98bf5d27177fcc60adab
Lucas Melo de Carvalho <lucas_m@id.uff.br>
2023-05-28 17:54:50 -0300

    uso da func os.walk

--- diretorios.py
+++ diretorios.py
@@ -1,6 +1,24 @@
+"""
+Faça um programa que leia uma string a qual representa um diretório. Gere um arquivo json com todos os arquivos deste diretório, dos seus diretórios filhos, dos filhos dos filhos, etc...
+formato json: combinação de lista e dicionário
+ITEM : nome, lista com todos os arquivos, diretórios(lista)
+diretório é uma lista de ITEM
+OS -> PESQUISAR
+"""
+
 import os
 import json
 
 path = input("Entre com o caminho do diretório: ")
-list_dir = os.listdir(path)
-print(list_dir)
+diretorio = list()
+item = dict()
+
+for dpath,dirs,files in os.walk(path):
+    print(dpath)
+    for d in dirs:
+        print(d)
+    for f in files:
+        print(f)
+
+#list_dir = os.listdir(path)
+#print(list_dir)
